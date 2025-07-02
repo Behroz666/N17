12d05ddd63b9d5ec7d08672caeaecd22037159e7
Behroz666 <BehrozBesharatnia@gmail.com>
2025-07-02 23:49:44 +0330

    remove unnecessary reponse variable

--- telegram.py
+++ telegram.py
@@ -9,7 +9,7 @@ def send_message(config, text):
         "parse_mode": "HTML",
         "disable_web_page_preview":True
     }
-    response = requests.post(url, data=payload)
+    requests.post(url, data=payload)
 
 def send_image(config, text, link):
     url = f"https://api.telegram.org/bot{config["Bot Token"]}/sendPhoto"
@@ -19,7 +19,7 @@ def send_image(config, text, link):
         "caption": text.replace("#",""),
         "parse_mode": "HTML"
     }
-    response = requests.post(url, data=payload)
+    requests.post(url, data=payload)
 
 def send_gallery(config, text, links):
     url = f"https://api.telegram.org/bot{config["Bot Token"]}/sendMediaGroup"
@@ -39,4 +39,4 @@ def send_gallery(config, text, links):
         "chat_id": config["Chat id"],
         "media": json.dumps(media) 
     }
-    response = requests.post(url, data=payload)
+    requests.post(url, data=payload)
