13adf23a8ae5b869abbaa5ca11fc472313726dda
Behroz666 <BehrozBesharatnia@gmail.com>
2025-08-17 17:45:35 +0330

    add 3 hour news for summaries

--- sub.py
+++ sub.py
@@ -11,6 +11,8 @@ if __name__ == "__main__":
         config = json.load(file)
 
     last_day_news = []
+    last_hours_news = []
+    news = []
 
     for page in config["pages"]:
         print(page)
@@ -18,25 +20,31 @@ if __name__ == "__main__":
         for tweet in tweets:
             now_local = datetime.now().astimezone()
             one_day_ago_utc = now_local.astimezone(timezone.utc) - timedelta(days=1)
+            three_hours_ago_uts = now_local.astimezone(timezone.utc) - timedelta(hours=3)
             if tweet[4] > one_day_ago_utc and all(x not in tweet[1] for x in ["🎙️", "🗣️"]):
                 last_day_news.append(tweet[1])
+            if tweet[4] > three_hours_ago_uts and all(x not in tweet[1] for x in ["🎙️", "🗣️"]):
+                last_hours_news.append(tweet[1])
         time.sleep(30)
     print(last_day_news)
-    text = summarize(config, last_day_news)
-    text = text.replace("**", "").replace("*   ", "")
-    if text.count(":") > 2:
-        final = ""
-        parts = text.split("\n\n")
-        for part in parts:
-            emoji = part[:1]
-            body = part.replace(emoji, "")
-            result = body.split(":", 1)[1].strip().replace("\n","")
-            final = final + emoji + " " + result + "\n\n"
-    else: 
-        final = text
-    message = "خلاصه اخبار امروز:\n\n" + final + "\n<a href='https://t.me/+QjvW46AcqcAwZjg8'>برای اخبار فوری و متن کامل مصاحبه ها به گپ ما بپیوندید</a>" + "\n\n@N17_Tottenham"
-    send_message(config, message, config["Summary Chat id"])
+    news.append(summarize(config, last_day_news))
+    time.sleep(60)
+    news.append(summarize(config, last_hours_news))
+    for text in news:
+        text = text.replace("**", "").replace("*   ", "")
+        if text.count(":") > 2:
+            final = ""
+            parts = text.split("\n\n")
+            for part in parts:
+                emoji = part[:1]
+                body = part.replace(emoji, "")
+                result = body.split(":", 1)[1].strip().replace("\n","")
+                final = final + emoji + " " + result + "\n\n"
+        else: 
+            final = text
+        message = "خلاصه اخبار امروز:\n\n" + final + "\n<a href='https://t.me/+QjvW46AcqcAwZjg8'>برای اخبار فوری و متن کامل مصاحبه ها به گپ ما بپیوندید</a>" + "\n\n@N17_Tottenham"
+        send_message(config, message, config["Summary Chat id"])
 
-    for admin in config["admins"]:
-        send_message(config, message, admin)
+        for admin in config["admins"]:
+            send_message(config, message, admin)
 
