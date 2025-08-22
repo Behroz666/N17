4eadd77559927bc1cedc2135c4f76adc95b64bf3
Behroz666 <BehrozBesharatnia@gmail.com>
2025-08-23 00:44:20 +0330

    add video download and upload support for twitter

--- main.py
+++ main.py
@@ -3,6 +3,21 @@ import time
 from rss import get_rss_feed
 from translate import translate
 from telegram import send_image, send_message, send_gallery, pin_message, delete_message
+import yt_dlp
+import os
+import requests
+
+def download_twitter_video(url, output_path):
+    # Options for lowest quality download
+    ydl_opts = {
+        'format': 'worst',  # 'worst' selects the lowest quality available
+        'outtmpl': output_path,  # output file name
+    }
+    
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(url, download=True)
+        filename = ydl.prepare_filename(info_dict)  # This gives the actual saved filename
+        return filename
 
 if __name__ == "__main__":
 
@@ -38,18 +53,19 @@ if __name__ == "__main__":
                     
                 if tweet[2] == 0:
                     limit = 4096
+                    url = f"https://x.com/{config["page"]}/status/{tweet[0]}#m"
                     if len(fa + tweet[1]) < (limit - 256):
-                        message = f"{fa}\n\n<blockquote expandable><a href='https://x.com/{config["page"]}/status/{tweet[0]}#m'>🇬🇧</a>: {tweet[1]}</blockquote>\n\n<a href='{"https://t.me/+2TG8ZxphObwzN2Q0"}'>VivaSpurs</a> | <a href='{"https://t.me/N17_Tottenham"}'>N17 Tottenham</a>"
+                        message = f"{fa}\n\n<blockquote expandable><a href='{url}'>🇬🇧</a>: {tweet[1]}</blockquote>\n\n<a href='{"https://t.me/+2TG8ZxphObwzN2Q0"}'>VivaSpurs</a> | <a href='{"https://t.me/N17_Tottenham"}'>N17 Tottenham</a>"
                     elif len(fa) < (limit - 456):
-                        message = f"{fa}\n\n<blockquote expandable><a href='https://x.com/{config["page"]}/status/{tweet[0]}#m'>🇬🇧</a>: {tweet[1][:(limit - 256 -len(fa))]}</blockquote>\n\n<a href='{"https://t.me/+2TG8ZxphObwzN2Q0"}'>VivaSpurs</a> | <a href='{"https://t.me/N17_Tottenham"}'>N17 Tottenham</a>"
+                        message = f"{fa}\n\n<blockquote expandable><a href='{url}'>🇬🇧</a>: {tweet[1][:(limit - 256 -len(fa))]}</blockquote>\n\n<a href='{"https://t.me/+2TG8ZxphObwzN2Q0"}'>VivaSpurs</a> | <a href='{"https://t.me/N17_Tottenham"}'>N17 Tottenham</a>"
                     else:
                         message = f"{fa[:(limit - 196)]}\n\n<a href='{"https://t.me/+2TG8ZxphObwzN2Q0"}'>VivaSpurs</a> | <a href='{"https://t.me/N17_Tottenham"}'>N17 Tottenham</a>"
                 else:
                     limit = 1024
                     if len(fa + tweet[1]) < (limit - 256):
-                        message = f"{fa}\n\n<blockquote expandable><a href='https://x.com/{config["page"]}/status/{tweet[0]}#m'>🇬🇧</a>: {tweet[1]}</blockquote>\n\n<a href='{"https://t.me/+2TG8ZxphObwzN2Q0"}'>VivaSpurs</a> | <a href='{"https://t.me/N17_Tottenham"}'>N17 Tottenham</a>"
+                        message = f"{fa}\n\n<blockquote expandable><a href='{url}'>🇬🇧</a>: {tweet[1]}</blockquote>\n\n<a href='{"https://t.me/+2TG8ZxphObwzN2Q0"}'>VivaSpurs</a> | <a href='{"https://t.me/N17_Tottenham"}'>N17 Tottenham</a>"
                     elif len(fa) < (limit - 456):
-                        message = f"{fa}\n\n<blockquote expandable><a href='https://x.com/{config["page"]}/status/{tweet[0]}#m'>🇬🇧</a>: {tweet[1][:(limit - 256 -len(fa))]}</blockquote>\n\n<a href='{"https://t.me/+2TG8ZxphObwzN2Q0"}'>VivaSpurs</a> | <a href='{"https://t.me/N17_Tottenham"}'>N17 Tottenham</a>"
+                        message = f"{fa}\n\n<blockquote expandable><a href='{url}'>🇬🇧</a>: {tweet[1][:(limit - 256 -len(fa))]}</blockquote>\n\n<a href='{"https://t.me/+2TG8ZxphObwzN2Q0"}'>VivaSpurs</a> | <a href='{"https://t.me/N17_Tottenham"}'>N17 Tottenham</a>"
                     else:
                         message = f"{fa[:(limit - 196)]}\n\n<a href='{"https://t.me/+2TG8ZxphObwzN2Q0"}'>VivaSpurs</a> | <a href='{"https://t.me/N17_Tottenham"}'>N17 Tottenham</a>"
 
@@ -58,7 +74,22 @@ if __name__ == "__main__":
                     message_id = send_message(config, message, config["Main Chat id"])
                 else:
                     if len(tweet[3]) == 1:
-                        message_id = send_image(config, message, tweet[3][0])
+                        if "video_thumb" not in tweet[3][0]:
+                            message_id = send_image(config, message, tweet[3][0])
+                        else:
+                            try:
+                                file_name = download_twitter_video(url, tweet[0])
+                                with open(file_name, 'rb') as f:
+                                    requests.post(
+                                        f"https://api.telegram.org/bot{config["Bot Token"]}/sendVideo",
+                                        files={'video': f},
+                                        data={'chat_id': config["Main Chat id"],
+                                            'caption': message,
+                                            "parse_mode": "HTML"}
+                                    )
+                                os.remove(file_name)
+                            except:
+                                message_id = send_image(config, message, tweet[3][0])
                     else:
                         message_id = send_gallery(config, message, tweet[3])
                 pin_message(config, message_id)
