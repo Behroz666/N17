c87799e3323cefdde10b0b7978c342f459faf337
Behroz666 <BehrozBesharatnia@gmail.com>
2025-11-27 18:35:25 +0330

    debug

--- rss.py
+++ rss.py
@@ -14,6 +14,7 @@ def get_rss_feed(page):
     feed = feedparser.parse(f"https://{RSS_URL}/{page}/rss")
     response = []
     if feed.bozo:
+        print(feed)
         print("There was an error parsing the feed.")
     else:
         for entry in feed.entries:
