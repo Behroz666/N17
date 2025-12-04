07960fcb8b85ab3f61737294f6721571b28e7c4d
Behroz666 <BehrozBesharatnia@gmail.com>
2025-12-04 18:31:11 +0330

    debug

--- new_main.py
+++ new_main.py
@@ -46,7 +46,9 @@ if __name__ == "__main__":
         response.raise_for_status()  # Raises an HTTPError for bad responses (4xx, 5xx)
         
         html_data = response.text
+        print(html_data)
         data = json.dumps(extract_posts_from_html(html_data))
+        print(data)
     except requests.exceptions.RequestException as e:
         print(f"Error fetching the URL: {e}")
     except Exception as e:
