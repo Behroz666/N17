58cf13a359d0acf2cff0b579f0834c9edb6d00e5
Behroz666 <BehrozBesharatnia@gmail.com>
2026-07-02 14:59:37 +0330

    short summary fix

--- mainy.py
+++ mainy.py
@@ -182,7 +182,10 @@ if posts_json:
             post_id = post["post id"]
             post_title = post["title"]
             post_summary = post["summary"]
-            text = text + f"<a href='{post_id}'>{post_title}</a>\n<blockquote expandable>{post_summary}</blockquote>\n\n"
+            if len(post_summary) > 15:
+                text = text + f"<a href='{post_id}'>{post_title}\n\n"
+            else:
+                text = text + f"<a href='{post_id}'>{post_title}</a>\n<blockquote expandable>{post_summary}</blockquote>\n\n"
         text  = text + "<a href='https://t.me/+QjvW46AcqcAwZjg8'>🔸 برای اخبار فوری و متن کامل مصاحبه ها به گپ ما بپیوندید</a>\n\n" + hyperlink
         message_id = send_message(config, text, config["Channel id"])
         telegram_done["done summary"].extend(telegram_done["summary"])
@@ -195,7 +198,10 @@ if posts_json:
             post_id = post["post id"]
             post_title = post["title"]
             post_summary = post["summary"]
-            text = text + f"<a href='{post_id}'>{post_title}</a>\n<blockquote expandable>{post_summary}</blockquote>\n\n"
+            if len(post_summary) > 15:
+                text = text + f"<a href='{post_id}'>{post_title}\n\n"
+            else:
+                text = text + f"<a href='{post_id}'>{post_title}</a>\n<blockquote expandable>{post_summary}</blockquote>\n\n"
         text  = text + "<a href='https://t.me/+QjvW46AcqcAwZjg8'>🔸 برای اخبار فوری و متن کامل مصاحبه ها به گپ ما بپیوندید</a>\n\n" + hyperlink
         message_id = send_message(config, text, config["Channel id"])
         telegram_done["summary done"].extend(telegram_done["summary"])
