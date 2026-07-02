067abffd17aa808fbb96135b23c58cb5669d7360
Behroz666 <BehrozBesharatnia@gmail.com>
2026-07-02 15:12:42 +0330

    put the tweet function back on work

--- mainy.py
+++ mainy.py
@@ -83,9 +83,19 @@ while True:
         if message_obj and message_obj.get("chat", {}).get("id") == TARGET_CHAT_ID and message_obj.get("from", {}).get("id") == 284403259 and message_obj.get("message_id") not in telegram_done["done"]:
             news_text = message_obj.get("text")[:-68]
             news_link = message_obj.get("text")[-68:].replace("rss.xcancel.com","x.com")
-            fa = ask_gemini(user_prompt=news_text, system_prompt=config["Translation System Prompt"])
-            send_message(config, f"news text: {news_text}\nnews link: {news_link}", 1140637004, preview= False)
-            telegram_done["done"].append(message_obj.get("message_id"))
+            if len(news_text) == 0 or news_text == "Gif" or str(news_text).startswith("R to @spurssglobal: football.london/tottenham-ho…"):
+                telegram_done["done"].append(message_obj.get("message_id"))
+            else:
+                if len(news_text) < 10:
+                    fa = news_text
+                elif (str(news_text).startswith("R to ") and len(news_text) < 30) or (str(news_text).startswith("RT by ") and len(news_text) < 30):
+                    fa = news_text
+                else:
+                    fa = ask_gemini(user_prompt=news_text, system_prompt=config["Translation System Prompt"])
+                message = f"{fa}\n\n<blockquote expandable><a href='{news_link}'>🇬🇧</a>: {news_text}</blockquote>\n\n{hyperlink}"
+                message_id = send_message(config, message, config["Main Chat id"], preview= True)
+                pin_message(config, message_id)
+                telegram_done["done"].append(message_obj.get("message_id"))
 
     break
 
