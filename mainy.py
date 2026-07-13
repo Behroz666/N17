6d2d1fca91d215425c932ec8b11bc17725dc12c9
Behroz666 <BehrozBesharatnia@gmail.com>
2026-07-13 22:23:51 +0330

    fix

--- mainy.py
+++ mainy.py
@@ -489,7 +489,7 @@ if posts_json:
                 text = text + f"<a href='{post_id}'>{post_title}</a>\n<blockquote expandable>{post_summary}</blockquote>\n\n"
         text  = text + "<a href='https://t.me/+QjvW46AcqcAwZjg8'>🔸 برای اخبار فوری و متن کامل مصاحبه ها به گپ ما بپیوندید</a>\n\n" + hyperlink
         print(f"text len: {len(text)}, image_urls: {image_urls}")
-        if len(text) < (1024 - 12 + 88*len(telegram_done["summary"] - 40) + 47 + 86) and len(image_urls) > 0:
+        if len(text) < (1024 - 12 + 88*len(telegram_done["summary"]) + 47 + 86 - 40) and len(image_urls) > 0:
             try:
                 text = "🗞️ اخبار :\n\n" + text
                 message_id = send_gallery(config, text, image_urls, id = config["Channel id"])
