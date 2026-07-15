4285fed08a19bc2efdd98073f778d1e68458d371
Behroz666 <BehrozBesharatnia@gmail.com>
2026-07-15 11:29:10 +0330

    update the styling

--- mainy.py
+++ mainy.py
@@ -405,9 +405,9 @@ if posts_json:
             except:
                 is_similar = False
             if len(post_summary) < 15 or (len(telegram_done["summary"]) > 15 and len(post_summary) > 150) or (len(telegram_done["summary"]) > 15 and len(post_summary) < 50) or (len(post_summary) < (len(post_title)*1.1)) or is_similar:
-                text = text + f"<a href='{post_id}'>{post_title}</a>\n\n"
+                text = text + f"- <b><a href='{post_id}'>{post_title}</a</b>n\n"
             else:
-                text = text + f"<a href='{post_id}'>{post_title}</a>\n<blockquote expandable>{post_summary}</blockquote>\n\n"
+                text = text + f"- <b><a href='{post_id}'>{post_title}</a></b>\n<blockquote expandable>{post_summary}</blockquote>\n\n"
         text  = text + "<a href='https://t.me/+QjvW46AcqcAwZjg8'>🔸 برای اخبار فوری و متن کامل مصاحبه ها به گپ ما بپیوندید</a>\n\n" + hyperlink
         print(f"text len: {len(text)}, image_urls: {image_urls}")
         if len(text) < (1024 - 12 + 88*len(telegram_done["summary"]) + 47 + 86 - 40) and len(image_urls) > 0:
@@ -443,9 +443,9 @@ if posts_json:
             except:
                 is_similar = False
             if len(post_summary) < 15 or (len(post_summary) < (len(post_title)*1.1)) or is_similar:
-                text = text + f"<a href='{post_id}'>{post_title}</a>\n\n"
+                text = text + f"- <b><a href='{post_id}'>{post_title}</a></b>\n\n"
             else:
-                text = text + f"<a href='{post_id}'>{post_title}</a>\n<blockquote expandable>{post_summary}</blockquote>\n\n"
+                text = text + f"- <b><a href='{post_id}'>{post_title}</a></b>\n<blockquote expandable>{post_summary}</blockquote>\n\n"
         text  = text + "<a href='https://t.me/+QjvW46AcqcAwZjg8'>🔸 برای اخبار فوری و متن کامل مصاحبه ها به گپ ما بپیوندید</a>\n\n" + hyperlink
         print(f"text len: {len(text)}, image_urls: {image_urls}")
         if len(text) < (1024 - 12 + 88*len(telegram_done["summary"]) + 47 + 86 - 40) and len(image_urls) > 0:
@@ -481,9 +481,9 @@ if posts_json:
             except:
                 is_similar = False
             if len(post_summary) < 15 or (len(post_summary) < (len(post_title)*1.1)) or is_similar:
-                text = text + f"<a href='{post_id}'>{post_title}</a>\n\n"
+                text = text + f"- <b><a href='{post_id}'>{post_title}</a></b>\n\n"
             else:
-                text = text + f"<a href='{post_id}'>{post_title}</a>\n<blockquote expandable>{post_summary}</blockquote>\n\n"
+                text = text + f"- <b><a href='{post_id}'>{post_title}</a></b>\n<blockquote expandable>{post_summary}</blockquote>\n\n"
         text  = text + "<a href='https://t.me/+QjvW46AcqcAwZjg8'>🔸 برای اخبار فوری و متن کامل مصاحبه ها به گپ ما بپیوندید</a>\n\n" + hyperlink
         print(f"text len: {len(text)}, image_urls: {image_urls}")
         if len(text) < (1024 - 12 + 88*len(telegram_done["summary"]) + 47 + 86 - 40) and len(image_urls) > 0:
